4ed82319bf9f6c4c2b806efd5533e473aeed7480
s30001 <s30001@pjwstk.edu.pl>
2025-03-06 21:31:31 +0100

    added method ArraysAvg

--- tutor1/tutor1/Program.cs
+++ tutor1/tutor1/Program.cs
@@ -1,9 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!!");
-Console.WriteLine("Hello, World!!!");
-Console.WriteLine("Hello, World!!!!");
-Console.WriteLine("Hello, World!!!!!");
-Console.WriteLine("Hello, World!!!!!!");
-Console.WriteLine("Hello, World!!!!!!!");
-Console.WriteLine("Hello, World!!!!!!!!");
+﻿using System.Linq;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!!");
+        Console.WriteLine("Hello, World!!!");
+        Console.WriteLine("Hello, World!!!!");
+        Console.WriteLine("Hello, World!!!!!");
+        Console.WriteLine("Hello, World!!!!!!");
+        Console.WriteLine("Hello, World!!!!!!!");
+        Console.WriteLine("Hello, World!!!!!!!!");
+        int[] ar = [1, 2, 3, 4, 5];
+        ArraysAvg(ar);
+}
+    static void ArraysAvg(int[] ar1)
+    {
+        double avg = ar1.Average();
+        Console.Write(avg);
+    }
+
+}
